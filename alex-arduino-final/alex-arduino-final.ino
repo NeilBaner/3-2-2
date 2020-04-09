f5a216e852fb71c8c12981477f381010891a335d
neilbaner <neilb5400@gmail.com>
2020-04-09 15:45:39 +0800

    added most comms routines

--- alex-arduino-final/alex-arduino-final.ino
+++ alex-arduino-final/alex-arduino-final.ino
@@ -143,14 +143,17 @@ void clearCounters(int which){
     }
 }
 
+
+
+
 // ISRs
 
 ISR(INT0_vect){
-
+    leftISR();
 }
 
 ISR(INT1_vect){
-
+    rightISR();
 }
 
 void leftISR(){
@@ -190,6 +193,10 @@ void rightISR(){
             break;
     }
 }
+
+
+
+
 
 // MOVEMENT ROUTINES
 
@@ -270,6 +277,122 @@ void stopAlex() {
     OCR1A = 0;
     OCR1B = 0;
 }
+
+
+
+
+// SERIAL ROUTINES
+
+int readSerial(){
+    int count = 0;
+    while (UCSR0A & 0b10000000 == 0b10000000){
+        buffer[count++] = UDR0;
+    }
+    return count;
+}
+
+void writeSerial(const char *buffer, int len){
+    int count = 0;
+    while(count < len){
+        while(UCSR0A & 0b0010000 == 0);
+        UDR0 = buffer[0];
+    }
+}
+// COMMUNICATION ROUTINES
+
+void sendMessage(const char *message) {
+    // Sends text messages back to the Pi. Useful
+    // for debugging.
+    TPacket messagePacket;
+    messagePacket.packetType = PACKET_TYPE_MESSAGE;
+    strncpy(messagePacket.data, message, MAX_STR_LEN);
+    sendResponse(&messagePacket);
+}
+
+void dbprint(char *format, ...) {
+    va_list args;
+    char buffer[128];
+    va_start(args, format);
+    vsprintf(buffer, format, args);
+    sendMessage(buffer);
+}
+
+
+
+void sendBadChecksum() {
+    // Tell the Pi that it sent us a packet with a bad
+    // checksum.
+
+    TPacket badChecksum;
+    badChecksum.packetType = PACKET_TYPE_ERROR;
+    badChecksum.command = RESP_BAD_CHECKSUM;
+    sendResponse(&badChecksum);
+}
+
+void sendBadCommand() {
+    // Tell the Pi that we don't understand its
+    // command sent to us.
+
+    TPacket badCommand;
+    badCommand.packetType = PACKET_TYPE_ERROR;
+    badCommand.command = RESP_BAD_COMMAND;
+    sendResponse(&badCommand);
+}
+
+void sendBadPacket() {
+    // Tell the Pi that it sent us a packet with a bad
+    // magic number.
+
+    TPacket badPacket;
+    badPacket.packetType = PACKET_TYPE_ERROR;
+    badPacket.command = RESP_BAD_PACKET;
+    sendResponse(&badPacket);
+}
+
+void sendBadResponse() {
+    TPacket badResponse;
+    badResponse.packetType = PACKET_TYPE_ERROR;
+    badResponse.command = RESP_BAD_RESPONSE;
+    sendResponse(&badResponse);
+}
+
+void sendOK() {
+    TPacket okPacket;
+    okPacket.packetType = PACKET_TYPE_RESPONSE;
+    okPacket.command = RESP_OK;
+    sendResponse(&okPacket);
+}
+
+void sendResponse(TPacket *packet) {
+    // Takes a packet, serializes it then sends it out
+    // over the serial port.
+    char buffer[PACKET_SIZE];
+    int len;
+
+    len = serialize(buffer, packet, sizeof(TPacket));
+    writeSerial(buffer, len);
+}
+
+void sendStatus() {
+    TPacket status;
+    status.command = COMMAND_GET_STATS;
+    status.packetType = PACKET_TYPE_RESPONSE;
+    status.params[0] = leftForwardTicks;
+    status.params[1] = rightForwardTicks;
+    status.params[2] = leftReverseTicks;
+    status.params[3] = rightReverseTicks;
+    status.params[4] = leftForwardTicksTurns;
+    status.params[5] = rightForwardTicksTurns;
+    status.params[6] = leftReverseTicksTurns;
+    status.params[7] = rightReverseTicksTurns;
+    status.params[8] = forwardDist;
+    status.params[9] = reverseDist;
+    sendResponse(&status);
+}
+
+
+
+
 
 // TEST ROUTINES
 
