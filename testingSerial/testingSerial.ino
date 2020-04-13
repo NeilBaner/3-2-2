8c9609f9a25131d5e1043451f69ad9a8a140cc8b
chuckiex3 <francenemooi@gmail.com>
2020-04-13 21:04:36 +0800

    doesn't work

--- testingSerial/testingSerial.ino
+++ testingSerial/testingSerial.ino
@@ -1,28 +1,7 @@
-#ifndef __CONTROL_H__
 #define __CONTROL_H__
 #include <stdint.h>
-// Packet types
-typedef enum
-{
-  PACKET_TYPE_COMMAND = 0,
-  PACKET_TYPE_RESPONSE = 1,
-  PACKET_TYPE_ERROR = 2,
-  PACKET_TYPE_MESSAGE = 3,
-  PACKET_TYPE_HELLO = 4
-} TPacketType;
-
-// Response types. This goes into the command field
-typedef enum
-{
-  RESP_OK = 0,
-  RESP_STATUS = 1,
-  RESP_BAD_PACKET = 2,
-  RESP_BAD_CHECKSUM = 3,
-  RESP_BAD_COMMAND = 4,
-  RESP_BAD_RESPONSE = 5
-} TResponseType;
-
 #define MAX_STR_LEN   32
+#define PACKET_SIZE 8
 // This packet has 1 + 1 + 2 + 32 + 16 * 4 = 100 bytes
 typedef struct
 {
@@ -33,33 +12,10 @@ typedef struct
   uint32_t params[16];
 } TPacket;
 
-// Commands
-// For direction commands, param[0] = distance in cm to move
-// param[1] = speed
-typedef enum
-{
-  COMMAND_FORWARD = 0,
-  COMMAND_REVERSE = 1,
-  COMMAND_TURN_LEFT = 2,
-  COMMAND_TURN_RIGHT = 3,
-  COMMAND_STOP = 4,
-  COMMAND_GET_STATS = 5,
-  COMMAND_CLEAR_STATS = 6
-} TCommandType;
-
-
-typedef enum {
-  STOP = 0,
-  FORWARD = 1,
-  BACKWARD = 2,
-  LEFT = 3,
-  RIGHT = 4
-} TDirection;
-#endif
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(5, OUTPUT);
   setupSerial();
   startSerial();
@@ -68,9 +24,9 @@ void setup() {
 // Setup serial comms, 9600 bps, 8N1, polling-based
 void setupSerial() {
   UCSR0C = 0b00000110; // 8 bits of data
-  UBRR0 = 103; 
-  //UCSR0A = 0b00000000;
-  UCSR0A = 0b10000000;
+  UBRR0 = 103;
+  UCSR0A = 0b00000000;
+  //UCSR0A = 0b10000000;
 }
 
 // Start Serial comms
@@ -78,14 +34,15 @@ void startSerial() {
   UCSR0B = 0b00011000;
 }
 
-int readSerial(char *buffer){
+int readSerial(char *buffer) {
   int count = 0;
   while (UCSR0A & 0b10000000 == 0b10000000) {
     buffer[count++] = UDR0;
-  return count;
+    return count;
+  }
 }
 
-TResult readPacket(TPacket *packet) {
+bool readPacket(TPacket * packet) {
   // Reads in data from the serial port and
   // deserializes it.Returns deserialized
   // data in "packet".
@@ -96,95 +53,23 @@ TResult readPacket(TPacket *packet) {
   len = readSerial(buffer);
 
   if (len == 0)
-    return PACKET_INCOMPLETE;
+    return false;
   else
-    return deserialize(buffer, len, packet);
+    return true;
 }
 
 void writeSerial(const char *buffer, int len) {
   int count = 0;
-    UDR0 = buffer[count++];
-  }
-}
-
-
-// COMMUNICATION ROUTINES
-
-void sendMessage(const char *message) {
-  // Sends text messages back to the Pi. Useful
-  // for debugging.
-  TPacket messagePacket;
-  messagePacket.packetType = PACKET_TYPE_MESSAGE;
-  strncpy(messagePacket.data, message, MAX_STR_LEN);
-  sendResponse(&messagePacket);
-}
-
-void dbprint(char *format, ...) {
-  va_list args;
-  char buffer[128];
-  va_start(args, format);
-  vsprintf(buffer, format, args);
-  sendMessage(buffer);
-}
-
-void sendBadChecksum() {
-  // Tell the Pi that it sent us a packet with a bad
-  // checksum.
-
-  TPacket badChecksum;
-  badChecksum.packetType = PACKET_TYPE_ERROR;
-  badChecksum.command = RESP_BAD_CHECKSUM;
-  sendResponse(&badChecksum);
-}
-
-void sendBadCommand() {
-  // Tell the Pi that we don't understand its
-  // command sent to us.
-
-  TPacket badCommand;
-  badCommand.packetType = PACKET_TYPE_ERROR;
-  badCommand.command = RESP_BAD_COMMAND;
-  sendResponse(&badCommand);
-}
-
-void sendBadPacket() {
-  // Tell the Pi that it sent us a packet with a bad
-  // magic number.
-
-  TPacket badPacket;
-  badPacket.packetType = PACKET_TYPE_ERROR;
-  badPacket.command = RESP_BAD_PACKET;
-  sendResponse(&badPacket);
-}
-
-void sendBadResponse() {
-  TPacket badResponse;
-  badResponse.packetType = PACKET_TYPE_ERROR;
-  badResponse.command = RESP_BAD_RESPONSE;
-  sendResponse(&badResponse);
-}
-
-void sendOK() {
-  TPacket okPacket;
-  okPacket.packetType = PACKET_TYPE_RESPONSE;
-  okPacket.command = RESP_OK;
-  sendResponse(&okPacket);
-}
-
-void sendResponse(TPacket *packet) {
-  // Takes a packet, serializes it then sends it out
-  // over the serial port.
-  char buffer[PACKET_SIZE];
-  int len = serialize(buffer, packet, sizeof(TPacket));
-  writeSerial(buffer, len);
+  UDR0 = buffer[count++];
 }
 
 void loop() {
   TPacket recvPacket;  // This holds commands from the Pi
-  TResult result = readPacket(&recvPacket);
-  if (result == PACKET_OK) {
-    handlePacket(&recvPacket);
-    digitalWrite(13, LOW);
-  } else 
-    digitalWrite(5, HIGH);
+  //TResult result = readPacket(&recvPacket);
+  bool result = readPacket(&recvPacket);
+  if (result) {
+    //handlePacket(&recvPacket);
+    digitalWrite(13, LOW); //if packet is received, light goes off
+  } else
+    digitalWrite(5, HIGH); //packet not received
 }
