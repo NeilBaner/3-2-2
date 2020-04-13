b615ce6fe08f4c0d4dc31e6826d6670f515afaa0
chuckiex3 <francenemooi@gmail.com>
2020-04-13 11:07:31 +0800

    Create testingSerial.ino
    wrote a lil code to test out serial code

---
+++ testingSerial/testingSerial.ino
@@ -0,0 +1,158 @@
+#ifndef __CONTROL_H__
+#define __CONTROL_H__
+
+#include <stdint.h>
+
+#define MAX_STR_LEN   32
+// This packet has 1 + 1 + 2 + 32 + 16 * 4 = 100 bytes
+typedef struct
+{
+  char packetType;
+  char command;
+  char dummy[2]; // Padding to make up 4 bytes
+  char data[MAX_STR_LEN]; // String data
+  uint32_t params[16];
+} TPacket;
+
+
+#endif
+// Packet types
+typedef enum
+{
+  PACKET_TYPE_COMMAND = 0,
+  PACKET_TYPE_RESPONSE = 1,
+  PACKET_TYPE_ERROR = 2,
+  PACKET_TYPE_MESSAGE = 3,
+  PACKET_TYPE_HELLO = 4
+} TPacketType;
+
+void setup() {
+  // put your setup code here, to run once:
+  setupSerial();
+  startSerial();
+}
+
+// Setup serial comms, 9600 bps, 8N1, polling-based
+void setupSerial() {
+  UCSR0C = 0b00000110;
+  UBRR0 = 103;
+  UCSR0A = 0b00000000;
+}
+
+// Start Serial comms
+void startSerial() {
+  UCSR0B = 0b00011000;
+}
+
+int readSerial(char *buffer){
+  int count = 0;
+  while (UCSR0A & 0b10000000 == 0b10000000) {
+    buffer[count++] = UDR0;
+  return count;
+}
+
+TResult readPacket(TPacket *packet) {
+  // Reads in data from the serial port and
+  // deserializes it.Returns deserialized
+  // data in "packet".
+
+  char buffer[PACKET_SIZE];
+  int len;
+
+  len = readSerial(buffer);
+
+  if (len == 0)
+    return PACKET_INCOMPLETE;
+  else
+    return deserialize(buffer, len, packet);
+}
+
+void writeSerial(const char *buffer, int len) {
+  int count = 0;
+    UDR0 = buffer[count++];
+  }
+}
+
+
+// COMMUNICATION ROUTINES
+
+void sendMessage(const char *message) {
+  // Sends text messages back to the Pi. Useful
+  // for debugging.
+  TPacket messagePacket;
+  messagePacket.packetType = PACKET_TYPE_MESSAGE;
+  strncpy(messagePacket.data, message, MAX_STR_LEN);
+  sendResponse(&messagePacket);
+}
+
+void dbprint(char *format, ...) {
+  va_list args;
+  char buffer[128];
+  va_start(args, format);
+  vsprintf(buffer, format, args);
+  sendMessage(buffer);
+}
+
+void sendBadChecksum() {
+  // Tell the Pi that it sent us a packet with a bad
+  // checksum.
+
+  TPacket badChecksum;
+  badChecksum.packetType = PACKET_TYPE_ERROR;
+  badChecksum.command = RESP_BAD_CHECKSUM;
+  sendResponse(&badChecksum);
+}
+
+void sendBadCommand() {
+  // Tell the Pi that we don't understand its
+  // command sent to us.
+
+  TPacket badCommand;
+  badCommand.packetType = PACKET_TYPE_ERROR;
+  badCommand.command = RESP_BAD_COMMAND;
+  sendResponse(&badCommand);
+}
+
+void sendBadPacket() {
+  // Tell the Pi that it sent us a packet with a bad
+  // magic number.
+
+  TPacket badPacket;
+  badPacket.packetType = PACKET_TYPE_ERROR;
+  badPacket.command = RESP_BAD_PACKET;
+  sendResponse(&badPacket);
+}
+
+void sendBadResponse() {
+  TPacket badResponse;
+  badResponse.packetType = PACKET_TYPE_ERROR;
+  badResponse.command = RESP_BAD_RESPONSE;
+  sendResponse(&badResponse);
+}
+
+void sendOK() {
+  TPacket okPacket;
+  okPacket.packetType = PACKET_TYPE_RESPONSE;
+  okPacket.command = RESP_OK;
+  sendResponse(&okPacket);
+}
+
+void sendResponse(TPacket *packet) {
+  // Takes a packet, serializes it then sends it out
+  // over the serial port.
+  char buffer[PACKET_SIZE];
+  int len = serialize(buffer, packet, sizeof(TPacket));
+  writeSerial(buffer, len);
+}
+
+void loop() {
+  TPacket recvPacket;  // This holds commands from the Pi
+  TResult result = readPacket(&recvPacket);
+  if (result == PACKET_OK) {
+    handlePacket(&recvPacket);
+  } else if (result == PACKET_BAD) {
+    sendBadPacket();
+  } else if (result == PACKET_CHECKSUM_BAD) {
+    sendBadChecksum();
+  }
+}
